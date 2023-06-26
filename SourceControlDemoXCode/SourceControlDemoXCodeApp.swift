f65589798e94a6b9308f3e473c1e27c576c07dc0
Aban <abantika.saha@oracle.com>
2023-06-26 11:37:29 +0530

    Initial Commit

---
+++ SourceControlDemoXCode/SourceControlDemoXCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlDemoXCodeApp.swift
+//  SourceControlDemoXCode
+//
+//  Created by Abantika Saha on 26/06/23.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlDemoXCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
